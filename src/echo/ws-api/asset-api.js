b731503d9736f48c3c988b1cd8e64d14d0a6f661
a.rudenko <a.rudenko@pixelplex.io>
2018-12-12 17:53:56 +0300

    Resolve EEX-25, EEX-26

--- src/echo/ws-api/asset-api.js
+++ src/echo/ws-api/asset-api.js
@@ -1,7 +1,38 @@
 class AssetAPI {
 
-	constructor(asset) {
-		this.asset = asset;
+	constructor(db) {
+		this.db = db;
+	}
+
+	/**
+	 *  @method getAssetHolders
+	 *  Retreive the information about the holders of the specified asset.
+	 *
+	 *  @param {String} assetId   [asset id to retreive]
+	 *  @param {Number} accountId [acount id to start retreiving from]
+	 *  @param {Number} limit     [count accounts (max 100)]
+	 */
+	getAssetHolders(assetId, accountId, limit) {
+		return this.db.exec('get_asset_holders', [assetId, accountId, limit]);
+	}
+
+	/**
+	 *  @method getAssetHoldersCount
+	 *  Retreive the number of holders of the provided asset.
+	 *
+	 *  @param {String} assetId   [asset id to retreive]
+	 */
+	getAssetHoldersCount(assetId) {
+		return this.db.exec('get_asset_holders_count', [assetId]);
+	}
+
+	/**
+	 *  @method getAssetHoldersCount
+	 *  Array of all asset IDs with the number of holders.
+	 *
+	 */
+	getAllAssetHolders() {
+		return this.db.exec('get_all_asset_holders', []);
 	}
 
 }
