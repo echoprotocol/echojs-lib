29166399a39a5a3bb9aa6bf3e907bd59dbc86a26
NickLatkovich <nicklatkovich@gmail.com>
2019-01-08 17:19:19 +0300

    PublicKeyType validator and appender to bytebuffer

--- src/serializer/basic-types/public-key-type.js
+++ src/serializer/basic-types/public-key-type.js
@@ -3,13 +3,24 @@ import PublicKey from '../../crypto/public-key';
 
 class PublicKeyType extends Type {
 
+	/**
+	 * @param {PublicKey|string} value
+	 * @returns {PublicKey}
+	 */
 	validate(value) {
-		if (value instanceof PublicKey) return;
+		if (value instanceof PublicKey) return value;
 		if (typeof value !== 'string') throw new Error('invalid publicKey type');
-		PublicKey.fromStringOrThrow(value);
+		return PublicKey.fromStringOrThrow(value);
 	}
 
-	appendToByteBuffer() { super.appendToByteBuffer(); }
+	/**
+	 * @param {PublicKey|string} value
+	 * @param {ByteBuffer} bytebuffer
+	 */
+	appendToByteBuffer(value, bytebuffer) {
+		value = this.validate(value);
+		bytebuffer.append(value.toBuffer().toString('binary'), 'binary');
+	}
 
 }
 
