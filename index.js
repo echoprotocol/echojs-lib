537294585da6e7f0d26bf57cd20b2420a4865c5b
NickLatkovich <nicklatkovich@gmail.com>
2019-01-12 13:26:38 +0300

    export Transaction in index

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import Echo from './src/echo/';
 
 import constants from './src/constants';
 import crypto from './src/crypto';
+import Transaction from './src/echo/transaction';
 
-export { Echo, constants, crypto };
+export { Echo, constants, crypto, Transaction };
 
 export default new Echo();
