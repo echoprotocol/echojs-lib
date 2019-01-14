8e00e8cdc23c3f9222025a34bf77e7988ce00edd
NickLatkovich <nicklatkovich@gmail.com>
2019-01-14 14:12:58 +0300

    Fix building

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* Chain */
+import Echo from './echo';
+
+import constants from './constants';
+import crypto from './crypto';
+import Transaction from './echo/transaction';
+
+export { Echo, constants, crypto, Transaction };
+
+export default new Echo();
