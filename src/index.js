40e9f566b90f4b57e38bba961bb021245e8273fb
a.rudenko <a.rudenko@pixelplex.io>
2019-01-21 13:41:27 +0300

    add utils export

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { PrivateKey, PublicKey } from './crypto';
 import Transaction from './echo/transaction';
 import echoReducer from './redux/reducer';
 
+export * from './utils/validators';
+export * from './utils/converters';
+
 export { OPERATIONS_IDS } from './constants';
 
 export { Echo, constants, PrivateKey, PublicKey, Transaction, echoReducer };
