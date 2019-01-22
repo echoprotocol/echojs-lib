bd2a5d3100e9ca93b1c1368d1b1e4e93091cd8c6
a.rudenko <a.rudenko@pixelplex.io>
2019-01-22 11:55:02 +0300

    fix exports

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import constants from './constants';
 import { PrivateKey, PublicKey } from './crypto';
 import Transaction from './echo/transaction';
 import echoReducer from './redux/reducer';
-
-export * from './utils/validators';
-export * from './utils/converters';
+import * as validators from './utils/validators';
+import * as converters from './utils/converters';
 
 export { OPERATIONS_IDS } from './constants';
 
-export { Echo, constants, PrivateKey, PublicKey, Transaction, echoReducer };
+export {
+	Echo,
+	constants,
+	PrivateKey,
+	PublicKey,
+	Transaction,
+	echoReducer,
+	validators,
+	converters,
+};
 
 export default new Echo();
