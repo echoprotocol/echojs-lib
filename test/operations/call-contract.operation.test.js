6204fb1e049e09c31a52dc10a5c98110c39fc100
mikhail.shautsou <mikhail.shautsou@gmail.com>
2019-01-28 14:19:18 +0300

    add call contract test

--- test/operations/call-contract.operation.test.js
+++ test/operations/call-contract.operation.test.js
@@ -1,7 +1,13 @@
 import "mocha";
-// import { Echo, OPERATIONS_IDS } from "../../src/index";
-import { Echo } from "../../src/index";
+import { ok } from "assert";
+
+import { Echo, constants } from "../../src/index";
+
 import { options } from "./create-contract.operation.test";
+
+import { privateKey, accountId } from "../_testAccount";
+
+const { OPERATIONS_IDS } = constants;
 
 describe('call contract', () => {
 
@@ -12,11 +18,26 @@ describe('call contract', () => {
 
 	after(() => echo.disconnect());
 
-	// it('successful', async () => {
-	// 	echo.createTransaction().addOperation(OPERATIONS_IDS.CALL_CONTRACT, {
-	// 		callee: options.contractAddress,
-	// 		// ...
-	// 	});
-	// });
+	it('successful', async () => {
+		const result = await echo.createTransaction()
+            .addOperation(OPERATIONS_IDS.CALL_CONTRACT, {
+                registrar: accountId,
+                value: {
+                    asset_id: '1.3.0',
+                    amount: 0,
+                },
+                gasPrice: 0,
+                gas: 1e7,
+                code: '86be3f80' + '0000000000000000000000000000000000000000000000000000000000000001', // setVariable(uint256)
+                callee: options.contractAddress,
+            })
+    	.addSigner(privateKey).broadcast();
+		const resultId = result[0].trx.operation_results[0][1];
+        ok(/^1\.17\.[1-9]\d*$/.test(resultId));
+
+		const contractResult = await echo.api.getContractResult(resultId);
+		const excepted = contractResult[1].exec_res.excepted;
+        ok(excepted === 'None');
+	}).timeout(1000 * 100);
 
 });
