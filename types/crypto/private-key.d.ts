716bf9913f09135bf86899d6c1ed47a9324da73d
Alina Lozyuk <a.lozyuk@pixelplex.io>
2019-06-27 19:25:22 +0300

    [ECHOJS-56] add method transform public key to buffer

--- types/crypto/private-key.d.ts
+++ types/crypto/private-key.d.ts
@@ -3,4 +3,15 @@ import PublicKey from "./public-key";
 export default class PrivateKey {
 	static fromWif(wif: string): PrivateKey;
 	toPublicKey(): PublicKey;
+	static fromBuffer(buf: Buffer): PrivateKey;
+	static fromSeed(seed: String): PrivateKey;
+	static fromWif(_privateWIF: String): PrivateKey;
+	toWif(): String;
+	toPublicKey(): PublicKey;
+	toBuffer(): Buffer;
+	getSharedSecret(): Buffer;
+	static fromHex(hex: String): PrivateKey;
+	toHex(): String;
+	toPrivateKeyString(): String;
+	static fromPrivateKeyString(privateKeyString: String): PrivateKey|null;
 };
--- types/crypto/public-key.d.ts
+++ types/crypto/public-key.d.ts
@@ -1,3 +1,14 @@
 export default class PublicKey {
 	toString(addressPrefix?: string): string;
+	static fromPublicKeyString(publicKey: string, addressPrefix?: string): PublicKey | null;
+	static fromBinary(bin: String): PublicKey;
+	static fromBuffer(buffer: Buffer): PublicKey;
+	toBuffer(): Buffer;
+	toBlockchainAddress(): String;
+	toPublicKeyString(addressPrefix?: String): String;
+	fromPublicKeyString(publicKey: String, addressPrefix?: String): PublicKey|null;
+	toByteBuffer(): ByteBuffer;
+	static fromHex(hex: String): PublicKey;
+	toHex(): String;
+	static fromPublicKeyStringHex(hex: String): PublicKey;
 };
