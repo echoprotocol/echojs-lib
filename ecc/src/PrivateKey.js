687290b43338bf55f2b6934e5a9f14ccc0ce61fa
Anastasia Rudenko <a.rudenko@pixelplex.io>
2019-07-19 18:22:41 +0300

    [ECHOJS-71] return to wif format

--- ecc/src/PrivateKey.js
+++ ecc/src/PrivateKey.js
@@ -1,5 +1,7 @@
 const BigInteger = require('bigi');
 const { encode, decode } = require('bs58');
+const deepEqual = require('deep-equal');
+const assert = require('assert');
 const ed25519 = require('ed25519.js');
 const { sha256 } = require('./hash');
 const PublicKey = require('./PublicKey');
@@ -46,11 +48,32 @@ class PrivateKey {
 
 	/** @return {string} Wallet const Format (still a secret, Not encrypted) */
 	static fromWif(_privateWIF) {
-		return PrivateKey.fromBuffer(decode(_privateWIF));
+		const privateWIF = Buffer.from(decode(_privateWIF));
+		const version = privateWIF.readUInt8(0);
+		assert.equal(0x80, version, `Expected version ${0x80}, instead got ${version}`);
+		// checksum includes the version
+		let privateKey = privateWIF.slice(0, -4);
+		const checksum = privateWIF.slice(-4);
+		let newChecksum = sha256(privateKey);
+		newChecksum = sha256(newChecksum);
+		newChecksum = newChecksum.slice(0, 4);
+		const isEqual = deepEqual(checksum, newChecksum); //	, 'Invalid checksum'
+		if (!isEqual) {
+			throw new Error('Checksum did not match');
+		}
+		privateKey = privateKey.slice(1);
+		return PrivateKey.fromBuffer(privateKey);
 	}
 
 	toWif() {
-		return encode(this.toBuffer());
+		let privateKey = this.toBuffer();
+		// checksum includes the version
+		privateKey = Buffer.concat([Buffer.from([0x80]), privateKey]);
+		let checksum = sha256(privateKey);
+		checksum = sha256(checksum);
+		checksum = checksum.slice(0, 4);
+		const privateWIF = Buffer.concat([privateKey, checksum]);
+		return encode(privateWIF);
 	}
 
 	toPublicKey() {
