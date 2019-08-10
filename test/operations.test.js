e9821ec4c240de50e66645f1615bbcc8fb1cbbec
a.pazniak <a.pazniak@pixelplex.io>
2019-08-10 17:34:03 +0300

    tests for subscriptions

--- test/operations.test.js
+++ test/operations.test.js
@@ -1,8 +1,8 @@
 
 describe('operations', () => {
-	// require('./operations/create-asset.operation.test');
-	// require('./operations/transfer.operation.test');
-	// require('./operations/create-contract.operation.test');
-	// require('./operations/call-contract.operation.test');
-	// require('./operations/account-update.operations.test');
+	require('./operations/create-asset.operation.test');
+	require('./operations/transfer.operation.test');
+	require('./operations/create-contract.operation.test');
+	require('./operations/call-contract.operation.test');
+	require('./operations/account-update.operations.test');
 });
