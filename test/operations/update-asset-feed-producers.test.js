eccfe0462881b202c4e6a1a296e696350bda1b56
NickLatkovich <n.latkovich@pixelplex.io>
2019-08-16 18:35:00 +0300

    [ECHOJS-94] Serializer tests (#18)
    * Fix "extensions" serialization
    * Fix nested operations serialization
    * Tests of:
      * field "extensions"
      * serialization errors of:
        * array
        * set
        * optional
        * static_variant
        * operation
        * serializable object
      * result of `SerializableSet#validate`

---
+++ test/operations/update-asset-feed-producers.test.js
@@ -0,0 +1,41 @@
+import { Echo } from "../../src";
+import { url, accountId } from "../_test-data";
+import { OPERATIONS_IDS } from "../../src/constants";
+import { fail, ok, strictEqual } from "assert";
+
+describe('update asset feed producers', () => {
+	const echo = new Echo();
+	before(async () => await echo.connect(url));
+	after(async () => await echo.disconnect());
+	describe('when element of "new_feed_producers" is not a string', () => {
+		/** @type {Error} */
+		let serializationError;
+		it('serialization should rejects', () => {
+			try {
+				echo.createTransaction().addOperation(OPERATIONS_IDS.ASSET_UPDATE_FEED_PRODUCERS, {
+					issuer: accountId,
+					asset_to_update: '1.3.0',
+					new_feed_producers: [null],
+				});
+			} catch (error) {
+				serializationError = error;
+				return;
+			}
+			fail('should rejects');
+		});
+		it('instance of Error', function () {
+			if (!serializationError) this.skip();
+			ok(serializationError instanceof Error);
+		});
+		const expectedErrorMessage = [
+			'operation with id 13',
+			'key "new_feed_producers"',
+			'set element with index 0',
+			'invalid id type',
+		].join(': ');
+		it(`with message "${expectedErrorMessage}"`, function () {
+			if (!serializationError || !(serializationError instanceof Error)) this.skip();
+			else strictEqual(serializationError.message, expectedErrorMessage);
+		});
+	});
+});
