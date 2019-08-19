a1bcdce8d8152bb37cfa61d09fec2a1d6f7f463d
NickLatkovich <nicklatkovich@gmail.com>
2019-08-19 14:28:55 +0300

    Remove contract deployment from pre-test script

--- test/preparation/preparation.js
+++ test/preparation/preparation.js
@@ -1,7 +1,5 @@
-import echo, { constants } from '../../src';
+import echo, { constants, OPERATIONS_IDS } from '../../src';
 import { url, privateKey, accountId } from './../_test-data';
-import { bytecode } from '../operations/_contract.test';
-import { strictEqual } from 'assert';
 
 const prepare = async () => {
 	await echo.connect(url, {
@@ -13,38 +11,18 @@ const prepare = async () => {
 		apis: constants.WS_CONSTANTS.CHAIN_APIS,
 	});
 	const balanceObject = await echo.api.getObject(`1.${constants.OBJECT_TYPES.BALANCE}.0`);
-
-	if (!balanceObject)
-		return;
-
+	if (!balanceObject) return;
+	/** @type {import("../../types/echo/transaction").default} */
 	const tx = echo.createTransaction();
-
-	const options = {
+	tx.addOperation(OPERATIONS_IDS.BALANCE_CLAIM, {
 		deposit_to_account: accountId,
 		balance_to_claim: `1.${constants.OBJECT_TYPES.BALANCE}.0`,
 		balance_owner_key: privateKey.toPublicKey().toString(),
 		total_claimed: balanceObject.balance,
-	};
-
-	tx.addOperation(constants.OPERATIONS_IDS.BALANCE_CLAIM, options);
-
-	tx.addSigner(privateKey);
-
-	await tx.broadcast(() => console.log('claim tx was broadcasted'));
-
-	const contractTx = echo.createTransaction();
-	contractTx.addOperation(constants.OPERATIONS_IDS.CONTRACT_CREATE, {
-		code: bytecode + '0123456789abcdeffedcba98765432100123456789abcdeffedcba9876543210',
-		eth_accuracy: false,
-		registrar: accountId,
-		value: { asset_id: `1.${constants.OBJECT_TYPES.ASSET}.0`, amount: 0 },
-	});
-
-	contractTx.addSigner(privateKey);
-
-	const broadcastingResult = await contractTx.broadcast(() => console.log('create contract tx was broadcasted'));
-	const operationResult = await echo.api.getContractResult(broadcastingResult[0].trx.operation_results[0][1]);
-	strictEqual(operationResult[1].exec_res.excepted, 'None', 'contract deployment failed');
+	}).addSigner(privateKey)
+	console.log('broadcasting claim tx...');
+	await tx.broadcast();
+	console.log('claim tx was broadcasted');
 };
 
 prepare()
