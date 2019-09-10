0e8a5bafd8fa661785da5a884295cfd31d0739b7
NickLatkovich <nicklatkovich@gmail.com>
2019-09-10 12:18:29 +0300

    varint32 deserializer

--- src/serializers/basic/integers/VarInt32.js
+++ src/serializers/basic/integers/VarInt32.js
@@ -1,3 +1,4 @@
+import BigNumber from 'bignumber.js';
 import IIntSerializer from './IIntSerializer';
 
 /** @typedef {import("bytebuffer")} ByteBuffer */
@@ -19,7 +20,28 @@ export default class VarInt32Serializer extends IIntSerializer {
 	 */
 	appendToByteBuffer(value, bytebuffer) {
 		const raw = this.toRaw(value);
+		if (raw < 0) throw new Error('negative varint32 serialization is not implemented');
 		bytebuffer.writeVarint32(raw);
+	}
+
+	/**
+	 * @param {Buffer} buffer
+	 * @param {number} [offset]
+	 * @returns {{ res: number, newOffset: number }}
+	 */
+	readFromBuffer(buffer, offset = 0) {
+		let result = new BigNumber(0);
+		let multiplier = new BigNumber(1);
+		let newOffset = offset;
+		while (true) {
+			const byte = buffer.readUInt8(newOffset);
+			const isEnd = byte < 0x80;
+			result = result.plus(new BigNumber(byte - (isEnd ? 0 : 0x80)).times(multiplier));
+			multiplier = multiplier.times(2 ** 7);
+			newOffset += 1;
+			if (isEnd) break;
+		}
+		return { res: this.toRaw(result), newOffset };
 	}
 
 }
