64934520507013ee428acca1197dbf4921fef69b
a.pazniak <a.pazniak@pixelplex.io>
2019-09-12 11:43:57 +0300

    serialized wallet api check

--- src/serializers/wallet.js
+++ src/serializers/wallet.js
@@ -1,6 +1,7 @@
 import { string as stringSerializer } from './basic';
 import { struct, vector } from './collections';
 
+// eslint-disable-next-line
 export const approvalDelta = struct({
 	active_approvals_to_add: vector(stringSerializer),
 	active_approvals_to_remove: vector(stringSerializer),
