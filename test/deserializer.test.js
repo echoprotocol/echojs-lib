15909e4cee7f2d288beab1f59c3d54c7145ac68a
NickLatkovich <nicklatkovich@gmail.com>
2019-09-12 18:15:11 +0300

    enable full testing

--- test/deserializer.test.js
+++ test/deserializer.test.js
@@ -1,7 +1,7 @@
 import { shouldReject } from './_test-utils';
 import { serializers } from '../'
 
-describe.only('deserializer', () => {
+describe('deserializer', () => {
 	describe('when first argument is not a buffer', () => {
 		shouldReject(() => serializers.operation.deserialize('not a buffer'), 'invalid buffer type');
 	});
