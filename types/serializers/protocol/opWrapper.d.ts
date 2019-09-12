5a5dc7fc2594d4ff43b9b10907407a04ffd22470
NickLatkovich <nicklatkovich@gmail.com>
2019-09-12 18:13:11 +0300

    op_wrapper deserializer

--- types/serializers/protocol/opWrapper.d.ts
+++ types/serializers/protocol/opWrapper.d.ts
@@ -2,7 +2,7 @@ import ISerializer, { SerializerInput, SerializerOutput } from '../ISerializer';
 import OperationSerializer, { TOperationInput, TOperationOutput } from '../operation';
 import OperationId from '../../interfaces/OperationId';
 
-export default class OperationWrapperSerializer extends ISerializer<
+export class OperationWrapperSerializer extends ISerializer<
 	TOperationInput<OperationId, boolean>,
 	TOperationOutput<OperationId>
 > {
@@ -11,4 +11,11 @@ export default class OperationWrapperSerializer extends ISerializer<
 	toRaw<T extends OperationId>(value: TOperationInput<T, false>): TOperationOutput<T>;
 	toRaw<T extends OperationId>(value: TOperationInput<T, true>, withUnrequiredFee: true): TOperationOutput<T>;
 	appendToByteBuffer<T extends OperationId>(value: TOperationInput<T, false>, bytebuffer: ByteBuffer): void;
+	readFromBuffer<T extends OperationId>(
+		buffer: Buffer,
+		offset?: number,
+	): { res: TOperationOutput<T>, newOffset: number };
 }
+
+declare const opWrapper: OperationWrapperSerializer;
+export default opWrapper;
