4f71b5c3a9be3091558d5eb14e5bdc1b303d1b50
a.pazniak <a.pazniak@pixelplex.io>
2019-09-16 18:18:55 +0300

    replace echojs-contract

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -317,3 +317,16 @@ export const isTimePointSec = (v) => {
 		return false;
 	}
 };
+
+const MAX_CONTRACT_ID = new BN(2).pow(152).minus(1);
+const CONTRACT_TYPE_ID = OBJECT_TYPES.CONTRACT;
+const ACCOUNT_TYPE_ID = OBJECT_TYPES.ACCOUNT;
+
+export const contractIdRegExp = new RegExp(`^1\\.${CONTRACT_TYPE_ID}\\.(([1-9]\\d*)|0)$`);
+export const addressRegExp = new RegExp(`^1\\.(${CONTRACT_TYPE_ID}|${ACCOUNT_TYPE_ID})\\.(([1-9]\\d*)|0)$`);
+
+export function checkContractId(contractId) {
+	if (!contractIdRegExp.test(contractId)) throw new Error('invalid contractId format');
+	const id = new BN(contractId.split('.')[2]);
+	if (id.gt(MAX_CONTRACT_ID)) throw new Error('contractId is greater than or equals to 2**152');
+}
