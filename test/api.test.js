c689fdad23def0d61656026e1b4b746a5d683327
Artsiom Dziaryba <a.dziaryba@pexelplex.io>
2019-10-10 17:52:14 +0300

    fix travis error

--- test/api.test.js
+++ test/api.test.js
@@ -820,7 +820,7 @@ describe('API', () => {
 					throw e;
 				}
 			})
-				.timeout(5001);
+				.timeout(5002);
 		});
 		describe('#lookupVoteIds()', () => {
 			it('should get vote by id and save to cache', async () => {
