46f96231c443950491a23cfed9912fe346a8fdc1
Artsiom Dziaryba <a.dziaryba@pexelplex.io>
2019-10-22 14:37:42 +0300

    [ECHOJS-220] remove register_account from api

--- types/echo/api.d.ts
+++ types/echo/api.d.ts
@@ -97,7 +97,6 @@ export default class Api {
 	lookupAccountNames(accountNames: Array<string>, force?: boolean): Promise<Array<Account>>;
 	lookupAssetSymbols(symbolsOrIds: Array<string>, force?: boolean): Promise<Array<Asset>>;
 	lookupCommitteeMemberAccounts(lowerBoundName: string, limit: number): Promise<any>;
-	registerAccount(name: string, activeKey: string, echoRandKey: string, wasBroadcastedCallback?: () => any): Promise<null>
 	requestRegistrationTask(): Promise<RegistrationTask>
 	validateTransaction(tr: Object): Promise<any>;
 	verifyAuthority(tr: Object): Promise<any>;
