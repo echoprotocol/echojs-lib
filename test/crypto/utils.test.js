ade893e4fae1868488120fa3f10a93f0d5c95a25
Vitaly Krasutsky <v.krasutsky@pixelplex.io>
2019-10-23 15:11:39 +0300

    refactor

--- test/crypto/utils.test.js
+++ test/crypto/utils.test.js
@@ -1,12 +1,14 @@
 import { strictEqual, throws } from 'assert';
 import { utils, PrivateKey } from '../../src/crypto';
+import { WIF, WIF2 } from '../_test-data';
 
 describe('Utils', () => {
 
 	describe('when we call signData', () => {
 
-		const privateKey1 = PrivateKey.fromWif('5JLi3M4H9qY3FMTyGW9TCC92ebbqNo36sUHwJqpKxvJMWN2XwbH')
-		const privateKey2 = PrivateKey.fromWif('5KkYp8qdQBaRmLqLz8WVrGjzkt7E13qVcr7cpdLowgJ1mjRyDx2')
+		const privateKey1 = PrivateKey.fromWif(WIF);
+		const privateKey2 = PrivateKey.fromWif(WIF2);
+		const buffer = Buffer.from('test', 'utf-8');
 
 		it('should throw an error when a first argument is wrong.', async () => {
 			throws(() => utils.signData('string', [privateKey1, privateKey2]), /invalid data type/);
@@ -27,22 +29,20 @@ describe('Utils', () => {
 		it('should return the expected result with one PrivateKey', async () => {
 
 			const expectedBuffer = '17e1ba550622dadaad4a16dd486c8a7fa1c9a90f3a11290dcf7f897777899807ea162f135355c4b229938f0991e9008e0ed0c15d75ce3b75f3c4b2e0b144de05';
-			const buffer = Buffer.from('test', 'utf-8');
-			const result = utils.signData(buffer, [PrivateKey.fromWif('5JLi3M4H9qY3FMTyGW9TCC92ebbqNo36sUHwJqpKxvJMWN2XwbH')]);
+			const result = utils.signData(buffer, [privateKey1]);
 
 			strictEqual(expectedBuffer, result.toString('hex'));
 
-		});	
+		});
 
 		it('should return the concatenated result with two PrivateKeys', async () => {
 
 			const expectedBuffer = '17e1ba550622dadaad4a16dd486c8a7fa1c9a90f3a11290dcf7f897777899807ea162f135355c4b229938f0991e9008e0ed0c15d75ce3b75f3c4b2e0b144de058b049d0c424f2214ffb13a4651ff8437bbcd9a58d9100533da11bf6133b7a16f226d9955cb3bea8752d085f639c215dc20095230072a06377adc53c471c8a605';
-			const buffer = Buffer.from('test', 'utf-8');
-			const result = utils.signData(buffer, [PrivateKey.fromWif('5JLi3M4H9qY3FMTyGW9TCC92ebbqNo36sUHwJqpKxvJMWN2XwbH'), PrivateKey.fromWif('5KkYp8qdQBaRmLqLz8WVrGjzkt7E13qVcr7cpdLowgJ1mjRyDx2')]);
+			const result = utils.signData(buffer, [privateKey1, privateKey2]);
 
 			strictEqual(expectedBuffer, result.toString('hex'));
 
-		});	
+		});
 
 	});
 
