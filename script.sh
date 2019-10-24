ef7343722e6012dd3670a7e34a869f6836f733ad
sergey ladutko <s.ladutko@pixelplex.io>
2019-10-24 17:13:04 +0300

    add script in travis

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+npx npm-login-cmd
+npm init --yes
+VERSION=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/-/ /' | awk '{print $2}')
+echo  $VERSION
+if  [ -z "$VERSION" ]
+then
+npm publish
+else
+npm publish --tag "$VERSION V"
+fi
