6bcb244365bddffe22af438d951df59adef0ac80
sergey ladutko <s.ladutko@pixelplex.io>
2019-10-28 15:28:35 +0300

    add login and rename  variable

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-npx npm-login-cmd
-VERSION=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/-/ /' | awk '{print $2}')
-echo  $VERSION
-if  [ -z "$VERSION" ]
+POSTFIXVERSION=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/-/ /' | awk '{print $2}')
+echo  $POSTFIXVERSION
+if  [ -z "$POSTFIXVERSION" ]
 then
 npm publish
 else
-npm publish --tag "$VERSION V"
+npm publish --tag "$POSTFIXVERSION V"
 fi
