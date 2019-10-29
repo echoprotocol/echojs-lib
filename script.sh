cd5f70c0af5ea1b6e277f6a218c1f41d4ab0c34f
sergey ladutko <s.ladutko@pixelplex.io>
2019-10-29 18:30:00 +0300

    script:V2

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-POSTFIXVERSION=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/-/ /' | awk '{print $2}')
-echo  $POSTFIXVERSION
-if  [ -z "$POSTFIXVERSION" ]
+VERSION=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/-/ /' | awk '{print $2}')
+if  cat package.json | grep echo > /dev/null
+then
+ECHOTEG=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/\-/\ /g' | awk '{print $2}')
+npm publish --tag "$ECHOTEG"
+elif [ -z "$VERSION" ]
 then
 npm publish
 else
-npm publish --tag "$POSTFIXVERSION V"
+npm publish --tag "rc"
 fi
