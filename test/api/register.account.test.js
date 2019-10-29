99f6f3f2887eddfada1a92954943df9da3ca7102
NickLatkovich <nicklatkovich@gmail.com>
2019-10-29 18:01:30 +0300

    [ECHOJS-232]

--- test/api/register.account.test.js
+++ test/api/register.account.test.js
@@ -1,11 +1,9 @@
-import { expect } from 'chai';
-import { inspect } from 'util';
+import { ok } from 'assert';
 
-import echo from '../../src';
-
+import echo from '../..';
 import { url } from '../_test-data';
 
-describe('API register account', () => {
+describe('API register account POW', () => {
 	before(async () => {
 		await echo.connect(url, {
 			connectionTimeout: 5000,
@@ -17,4 +15,23 @@ describe('API register account', () => {
 		});
 
 	});
+
+	after(async () => await echo.disconnect());
+
+	describe.only('register account pow', () => {
+		it('register account', async () => {
+
+				const result = await echo.api.registerAccount(
+					'kokoko'+ Date.now(),
+					'ECHODvHDsAfk2M8LhYcxLZTbrNJRWT3UH5zxdaWimWc6uZkH',
+					'ECHODvHDsAfk2M8LhYcxLZTbrNJRWT3UH5zxdaWimWc6uZkH',
+					() => {
+						console.log('was broadcasted');
+					}
+				)
+
+				ok(Array.isArray(result));
+		}).timeout(1e8);
+	});
+
 });
