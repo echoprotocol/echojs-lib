284f7614ab51fe1ff8aea3c1829f6e1c6e118749
SergeyLadutko <40435115+SergeyLadutko@users.noreply.github.com>
2019-11-01 10:49:02 +0300

    [DEVOPS-30] add script in travis (#124)

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VERSION=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/-/ /' | awk '{print $2}')
+if  cat package.json | grep echo > /dev/null
+then
+ECHOTEG=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/\-/\ /g' | awk '{print $2}')
+npm publish --tag "$ECHOTEG"
+elif [ -z "$VERSION" ]
+then
+npm publish
+else
+npm publish --tag "rc"
+fi
