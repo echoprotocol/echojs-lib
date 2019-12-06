db0621d31a09edd4760b907d774312ecb2da8efa
dima <d.karpushkin@pixelplex.io>
2019-12-06 14:38:05 +0300

    fix

--- test/_test-data.js
+++ test/_test-data.js
@@ -2,7 +2,7 @@ import { PrivateKey, constants } from "../";
 
 export const privateKey = PrivateKey.fromWif('5KkYp8qdQBaRmLqLz8WVrGjzkt7E13qVcr7cpdLowgJ1mjRyDx2');
 export const accountName = 'nathan';
-export const accountId = `1.${constants.PROTOCOL_OBJECT_TYPE_ID.ACCOUNT}.10`;
+export const accountId = `1.${constants.PROTOCOL_OBJECT_TYPE_ID.ACCOUNT}.6`;
 export const contractId = `1.${constants.PROTOCOL_OBJECT_TYPE_ID.CONTRACT}.0`;
 
 export const url = 'ws://127.0.0.1:6311';
