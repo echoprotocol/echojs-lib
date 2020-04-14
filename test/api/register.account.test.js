d97082eff3260050922433749580b827dcbd4921
NickLatkovich <nicklatkovich@gmail.com>
2020-04-14 16:17:42 +0300

    Fix registration test

--- test/api/register.account.test.js
+++ test/api/register.account.test.js
@@ -6,7 +6,7 @@ import { url } from '../_test-data';
 describe('API register account POW', () => {
 	before(async () => {
 		await echo.connect(url, {
-			connectionTimeout: 5000,
+			connectionTimeout: 30000,
 			maxRetries: 5,
 			pingTimeout: 3000,
 			pingDelay: 10000,
@@ -19,22 +19,32 @@ describe('API register account POW', () => {
 	after(async () => await echo.disconnect());
 
 	describe('register account', () => {
-		it('register account', async () => {
-			let isResolved = false;
-			const promise = echo.api.registerAccount(
-				'kokoko'+ Date.now(),
-				'ECHODvHDsAfk2M8LhYcxLZTbrNJRWT3UH5zxdaWimWc6uZkH',
-				'ECHODvHDsAfk2M8LhYcxLZTbrNJRWT3UH5zxdaWimWc6uZkH',
-				null,
-				() => isResolved = true,
-			);
-			await new Promise((resolve) => setTimeout(() => resolve(), 100));
-			await echo.api.getAccountCount();
-			ok(!isResolved);
-			const result = await promise;
-			ok(isResolved);
-			ok(Array.isArray(result));
-		}).timeout(1e8);
+		it('register account', () => new Promise(async (resolve, reject) => {
+			try {
+				let isResolved = false;
+				let isPromiseRejectionHandled = false;
+				const promise = echo.api.registerAccount(
+					'kokoko'+ Date.now(),
+					'ECHODvHDsAfk2M8LhYcxLZTbrNJRWT3UH5zxdaWimWc6uZkH',
+					'ECHODvHDsAfk2M8LhYcxLZTbrNJRWT3UH5zxdaWimWc6uZkH',
+					null,
+					() => isResolved = true,
+				).catch((err) => {
+					if (isPromiseRejectionHandled) throw err;
+					reject(err);
+				});
+				await new Promise((resolve) => setTimeout(() => resolve(), 100));
+				await echo.api.getAccountCount();
+				ok(!isResolved);
+				isPromiseRejectionHandled = true;
+				const result = await promise;
+				ok(isResolved);
+				ok(Array.isArray(result));
+				resolve();
+			} catch (err) {
+				reject(err);
+			}
+		})).timeout(1e8);
 	});
 
 });
