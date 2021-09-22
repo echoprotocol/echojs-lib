4800d02d424662eaa570d6f32b57c0d2bd3c22e7
NickLatkovich <nicklatkovich@gmail.com>
2021-09-22 13:46:19 +0300

    Fix pair serializer

--- src/serializers/collections/Pair.js
+++ src/serializers/collections/Pair.js
@@ -1,58 +1,97 @@
 import ISerializer from '../ISerializer';
 
+/** @typedef {import("bytebuffer")} ByteBuffer */
+
+/**
+ * @template {ISerializer} T
+ * @typedef {import("../ISerializer").SerializerInput<T>} SerializerInput
+ */
+
+/**
+ * @template {ISerializer} T
+ * @typedef {import("../ISerializer").SerializerOutput<T>} SerializerOutput
+ */
+
+/**
+ * @template {ISerializer} TFirst
+ * @template {ISerializer} TSecond
+ * @typedef {[SerializerInput<TFirst>, SerializerInput<TSecond>]} TInput
+ */
+
+/**
+ * @template {ISerializer} TFirst
+ * @template {ISerializer} TSecond
+ * @typedef {[SerializerOutput<TFirst>, SerializerOutput<TSecond>]} TOutput
+ */
+
+/**
+ * @template {ISerializer} TFirst
+ * @template {ISerializer} TSecond
+ * @augments {ISerializer<TInput<TFirst, TSecond>, TOutput<TFirst, TSecond>>}
+ */
 export default class PairSerializer extends ISerializer {
-
 	get firstSerializer() { return this._firstSerializer; }
-
 	get secondSerializer() { return this._secondSerializer; }
 
-
+	/**
+	 * @param {TFirst} firstSerializer
+	 * @param {TSecond} secondSerializer
+	 */
 	constructor(firstSerializer, secondSerializer) {
 		if (!(firstSerializer instanceof ISerializer)) throw new Error('key is not serializer');
 		if (!(firstSerializer instanceof ISerializer)) throw new Error('value is not serializer');
 		super();
 		/**
 		 * @private
-		 * @type {TKey}
+		 * @type {TFirst}
 		 */
 		this._firstSerializer = firstSerializer;
 		/**
 		 * @private
-		 * @type {TValue}
+		 * @type {TSecond}
 		 */
 		this._secondSerializer = secondSerializer;
 	}
 
+	/**
+	 * @param {TInput<TFirst, TSecond>} value
+	 * @returns {TOutput<TFirst, TSecond>}
+	 */
 	toRaw(value) {
 		const raw = new Array(2);
 		try {
 			raw[0] = this.firstSerializer.toRaw(value[0]);
+		} catch (error) {
+			throw new Error(`pair[0]: ${error.message}`);
+		}
+		try {
 			raw[1] = this.secondSerializer.toRaw(value[1]);
 		} catch (error) {
-			throw new Error(`Unable to serialize pair: ${error.message}`);
+			throw new Error(`pair[1]: ${error.message}`);
 		}
 		return raw;
 	}
 
+	/**
+	 * @param {TInput<TFirst, TSecond>} value
+	 * @param {ByteBuffer} bytebuffer
+	 */
 	appendToByteBuffer(value, bytebuffer) {
 		const raw = this.toRaw(value);
 		this.firstSerializer.appendToByteBuffer(raw[0], bytebuffer);
 		this.secondSerializer.appendToByteBuffer(raw[1], bytebuffer);
 	}
 
+	/**
+	 * 
+	 * @param {Buffer} buffer
+	 * @param {number} [offset]
+	 * @returns {{ res: TOutput<TFirst, TSecond>, newOffset: number }}
+	 */
 	readFromBuffer(buffer, offset = 0) {
-		const result = [];
-		let it = offset;
-
-		const { res: firstElement, newOffset: intermediateOffset } = this.firstSerializer.readFromBuffer(buffer, it);
-		result.push(firstElement);
-		it = intermediateOffset;
-
-		const { res: secondElement, newOffset } = this.secondSerializer.readFromBuffer(buffer, it);
-		result.push(secondElement);
-		it = newOffset;
-
-		return { res: result, newOffset: it };
+		const { res: firstElement, newOffset: intermediateOffset } = this.firstSerializer.readFromBuffer(buffer, offset);
+		const { res: secondElement, newOffset } = this.secondSerializer.readFromBuffer(buffer, intermediateOffset);
+		return { res: [firstElement, secondElement], newOffset };
 	}
 
 }
--- types/serializers/collections/Pair.d.ts
+++ types/serializers/collections/Pair.d.ts
@@ -3,15 +3,14 @@ import ISerializer, { SerializerInput, SerializerOutput } from "../ISerializer";
 type TInput<T extends ISerializer, K extends ISerializer> = [SerializerInput<T>, SerializerInput<K>];
 type TOutput<T extends ISerializer, K extends ISerializer> = [SerializerOutput<T>, SerializerOutput<K>];
 
-
 export default class PairSerializer<TFirstSerializer extends ISerializer, TSecondSerializer extends ISerializer> extends ISerializer<
-  TInput<TFirstSerializer, TSecondSerializer>,
-  TOutput<TFirstSerializer, TSecondSerializer>
-  > {
-  readonly firstSerializer: TFirstSerializer;
-  readonly secondSerializer: TSecondSerializer;
-  constructor(firstSerializer: TFirstSerializer, secondSerializer: TSecondSerializer);
-  toRaw(value: TInput<TFirstSerializer, TSecondSerializer>): TOutput<TFirstSerializer, TSecondSerializer>;
-  appendToByteBuffer(value: TInput<TFirstSerializer, TSecondSerializer>, bytebuffer: ByteBuffer): void;
-  readFromBuffer(buffer: Buffer, offset?: number): { res: TOutput<TFirstSerializer, TSecondSerializer>, newOffset: number };
+	TInput<TFirstSerializer, TSecondSerializer>,
+	TOutput<TFirstSerializer, TSecondSerializer>
+> {
+	readonly firstSerializer: TFirstSerializer;
+	readonly secondSerializer: TSecondSerializer;
+	constructor(firstSerializer: TFirstSerializer, secondSerializer: TSecondSerializer);
+	toRaw(value: TInput<TFirstSerializer, TSecondSerializer>): TOutput<TFirstSerializer, TSecondSerializer>;
+	appendToByteBuffer(value: TInput<TFirstSerializer, TSecondSerializer>, bytebuffer: ByteBuffer): void;
+	readFromBuffer(buffer: Buffer, offset?: number): { res: TOutput<TFirstSerializer, TSecondSerializer>, newOffset: number };
 }
